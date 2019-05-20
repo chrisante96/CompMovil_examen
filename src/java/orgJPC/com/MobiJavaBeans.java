e4526d39f04d1359f702c00211284b471f45354c
ANTEZANA <ANTEZANA@CHRISTIAN>
2019-05-20 14:44:58 +0200

    mayor de 3 numeros enteros

--- src/java/orgJPC/com/MobiJavaBeans.java
+++ src/java/orgJPC/com/MobiJavaBeans.java
@@ -1,51 +1,77 @@
 package orgJPC.com;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
+import javax.swing.JOptionPane;
+
 @ManagedBean
 @RequestScoped
 public class MobiJavaBeans {
-int n1;
-int n2;
-int n3;
-int max;
-public MobiJavaBeans() {
-}
-public void mayor(){
-if(n1>n2 && n1>n3)
-   max=n1; 
-else{
-    if(n2>n1 && n2>n3)
-        max=n2;
-   else
-        max=n3;
-}
-FacesContext c = FacesContext.getCurrentInstance();
-c.addMessage(null, new FacesMessage("Mayor de tres Numeros","Mayor= "+max));
-}
-public int getN1() {
-return n1;
-}
-public void setN1(int n1) {
-this.n1 = n1;
-}
-public int getN2() {
-return n2;
-}
-public void setN2(int n2) {
-this.n2 = n2;
-}
-public int getN3() {
-return n3;
-}
-public void setN3(int n3) {
-this.n3 = n3;
-}
-public int getMax() {
-return max;
-}
-public void setMax(int max) {
-this.max = max;
-}
+
+    int Mayor = 0;
+    int num1 = 0;
+    int num2 = 0;
+    int num3 = 0;
+    
+    public MobiJavaBeans() {
+    }
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    public int getNum3() {
+        return num3;
+    }
+
+    public void setNum3(int num3) {
+        this.num3 = num3;
+    }
+    public void setlimpiar() {
+        this.num1=0;
+        this.num2=0;
+        this.num3=0;
+    }
+
+    public int getmayor() {
+        return Mayor;
+    }
+
+    public void setmayor(int Mayor) {
+        this.Mayor = Mayor;
+    }
+
+    public void mayor() {
+        if (num1 > num2 && num1 > num3) {
+            Mayor = num1;
+        } else {
+            if (num2 > num1 && num2 > num3) {
+                Mayor = num2;
+            } else {
+                Mayor = num3;
+            }
+        }
+        FacesContext c = FacesContext.getCurrentInstance();
+        c.addMessage(null, new FacesMessage("EL MAYOR ES:" +Mayor));
+        
+    }
+
+    private void num1(String _) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
